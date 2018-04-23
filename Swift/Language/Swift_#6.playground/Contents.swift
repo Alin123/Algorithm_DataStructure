40609c5a6cd5f8ca10a152668601c2db654957a8
Alin123 <1149433193@qq.com>
2018-04-23 09:26:05 +0800

    内存安全和高级运算符

--- Swift/Language/Swift_#6.playground/Contents.swift
+++ Swift/Language/Swift_#6.playground/Contents.swift
@@ -89,3 +89,126 @@ func someFunction() {
     var oscar = Player(name: "Oscar", health: 10, energy: 10)
     balance(&oscar.health, &oscar.energy)  // 正常
 }
+
+
+//## 高级运算符
+//#### 位运算符
+//###### 按位取反运算符～
+let initialBits: UInt8 = 0b00001111
+let invertedBits = ~initialBits
+//###### 按位与运算符&
+let firstSixBits: UInt8 = 0b11111100
+let lastSixBits:  UInt8 = 0b00111111
+if firstSixBits & lastSixBits == 0b00111100 {
+    print("对应位都是1才为1，否则为0")
+}
+//###### 按位或运算符
+let someBits: UInt8 =     0b10110010
+let moreBits: UInt8 =     0b01011110
+if someBits | moreBits == 0b11111110 {
+    print("对应位只要有1就为1，否则为0")
+}
+
+//###### 按位异或^
+let firstBits: UInt8 =      0b00010100
+let otherBits: UInt8 =      0b00000101
+if firstBits ^ otherBits == 0b00010001 {
+    print("对应位不同才为1，否则为0")
+}
+
+//#### 按位左移、右移运算符
+//###### 无符号整数的移位运算
+// 对无符号整数进行移位的规则如下：
+
+//1. 已经存在的位按指定的位数进行左移和右移。
+//2. 任何因移位而超出整数存储范围的位都会被丢弃。
+//3. 用0来填充移位后产生的空白位。
+
+let shiftBits: UInt8 = 4
+shiftBits << 1
+shiftBits << 2
+shiftBits << 5
+shiftBits << 6
+shiftBits >> 2
+
+// 可以使用移位运算对其他的数据类型进行编码和解码：
+let pink: UInt32 = 0xCC6699
+let redComponent   = (pink & 0xFF0000) >> 16 // 0xCC
+let greenComponent = (pink & 0x00FF00) >>  8 // 0x66
+let blueComponet   = (pink & 0x0000FF) >>  0 // 0x99
+
+//###### 有符号整数的移位运算，有符号整数使用第1个比特位通常被称为符号位来表示这个数的正负。符号0表示正数，为1表示负数（负数的表示方法：二机制补码）。
+//规则：
+
+//1. 右移：在高位填充符号位
+//2. 左移，符号位不变，在低位填充0
+
+
+//#### 溢出运算符：&+、&-，&*
+//#### 优先级和结合性
+//#### 运算符函数
+struct Vector2D {
+    var x = 0.0, y = 0.0
+}
+extension Vector2D {
+    static func + (left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y + right.y)
+    }
+}
+
+let vector = Vector2D(x: 3.0, y: 1.0)
+let anotherVector = Vector2D(x: 2.0, y: 4.0)
+let combineVector = vector + anotherVector
+
+//###### 前缀和后缀运算符
+extension Vector2D {
+    static prefix func - (vector: Vector2D) -> Vector2D {
+        return Vector2D(x: -vector.x, y: -vector.y)
+    }
+}
+
+let positive = Vector2D(x: 3.0, y: 4.0)
+let negative = -positive
+//###### 符合赋值运算符
+extension Vector2D {
+    static func += (left: inout Vector2D, right: Vector2D) {
+        left = left + right
+    }
+}
+
+//###### 等价运算符
+extension Vector2D {
+    static func == (left: Vector2D, right: Vector2D) -> Bool {
+        return left.x == right.x && left.y == right.y
+    }
+    
+    static func !=  (left: Vector2D, right: Vector2D) -> Bool {
+        return !(left == right)
+    }
+}
+
+//#### 自定义运算符
+prefix operator +++
+extension Vector2D {
+    static prefix func +++ (vector: inout Vector2D) -> Vector2D {
+        vector += vector
+        return vector
+    }
+}
+var toBeDoubled = Vector2D(x: 1.0, y: 4.0)
+let afterDoubing = +++toBeDoubled
+
+//###### 自定义中缀运算符的优先级
+infix operator +-: AdditionPrecedence
+extension Vector2D {
+    static func +- (left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y - right.y)
+    }
+}
+
+
+
+
+
+
+
